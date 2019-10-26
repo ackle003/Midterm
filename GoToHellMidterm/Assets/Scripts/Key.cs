6974d3d4687a622da5a56d3ca4529232d0140750
allieyo <57029163+allieyo@users.noreply.github.com>
2019-10-25 23:59:05 -0700

    Go To Hell Game
    Go To Hell Game

---
+++ GoToHellMidterm/Assets/Scripts/Key.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Key : MonoBehaviour {
+
+    public bool hasKey = false;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+
+        hasKey = true;
+        Debug.Log(hasKey);
+        gameObject.GetComponent<SpriteRenderer>().enabled = false;
+
+    }
+
+}
