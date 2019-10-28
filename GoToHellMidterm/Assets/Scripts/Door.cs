1a3560c1da8cf1b527529a66f68a9bdff7b887f8
ackle003 <50431311+ackle003@users.noreply.github.com>
2019-10-28 14:54:15 -0700

    Update Door.cs

--- GoToHellMidterm/Assets/Scripts/Door.cs
+++ GoToHellMidterm/Assets/Scripts/Door.cs
@@ -2,17 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
+// class which defines door behaviour 
 public class Door : MonoBehaviour {
 
-    GameObject obj;
-
+    GameObject obj; // door in game acts as mechanism for scene victory
+    
     private void OnTriggerStay2D(Collider2D other)
     {
+        // check if the character has attained the key within scene
         obj = GameObject.FindGameObjectWithTag("Key");
         bool openDoor = obj.GetComponent<Key>().hasKey;
         Debug.Log(openDoor);
-
+        // The user has beat the level if they have pressed down when atop the door and have previously collided with key
         if(Input.GetKeyDown(KeyCode.UpArrow) && openDoor == true)
         {
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
