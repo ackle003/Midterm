f2439631bdd569c0e2a47edf9fc6b3d22bc4a38d
ackle003 <50431311+ackle003@users.noreply.github.com>
2019-10-28 14:48:00 -0700

    Update Camera2DFollow.cs

--- GoToHellMidterm/Assets/Scripts/Camera2DFollow.cs
+++ GoToHellMidterm/Assets/Scripts/Camera2DFollow.cs
@@ -3,20 +3,24 @@ using UnityEngine;
 
 namespace UnityStandardAssets._2D
 {
+    // Script for camera control. 
     public class Camera2DFollow : MonoBehaviour
     {
+        // Instances which control camera speed and movement
         public Transform target;
         public float damping = 1;
         public float lookAheadFactor = 3;
         public float lookAheadReturnSpeed = 0.5f;
         public float lookAheadMoveThreshold = 0.1f;
-
+        
+        // Instances which control the direction of the camera
         private float m_OffsetZ;
         private Vector3 m_LastTargetPosition;
         private Vector3 m_CurrentVelocity;
         private Vector3 m_LookAheadPos;
-
+        
         // Use this for initialization
+        // Initializes character position 
         private void Start()
         {
             m_LastTargetPosition = target.position;
@@ -30,9 +34,9 @@ namespace UnityStandardAssets._2D
         {
             // only update lookahead pos if accelerating or changed direction
             float xMoveDelta = (target.position - m_LastTargetPosition).x;
-
+            
             bool updateLookAheadTarget = Mathf.Abs(xMoveDelta) > lookAheadMoveThreshold;
-
+            // determine if movement occurs and update camera position if so
             if (updateLookAheadTarget)
             {
                 m_LookAheadPos = lookAheadFactor*Vector3.right*Mathf.Sign(xMoveDelta);
@@ -41,7 +45,7 @@ namespace UnityStandardAssets._2D
             {
                 m_LookAheadPos = Vector3.MoveTowards(m_LookAheadPos, Vector3.zero, Time.deltaTime*lookAheadReturnSpeed);
             }
-
+            // set the camera to the new position and update position
             Vector3 aheadTargetPos = target.position + m_LookAheadPos + Vector3.forward*m_OffsetZ;
             Vector3 newPos = Vector3.SmoothDamp(transform.position, aheadTargetPos, ref m_CurrentVelocity, damping);
 
