8997c57faef5fd592ebe8bfb90c7c572bf457387
ackle003 <50431311+ackle003@users.noreply.github.com>
2019-10-28 14:58:01 -0700

    Update LoadScene.cs

--- GoToHellMidterm/Assets/Scripts/LoadScene.cs
+++ GoToHellMidterm/Assets/Scripts/LoadScene.cs
@@ -2,16 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
+// Scene management class responsible for transitioning scenes
 public class LoadScene : MonoBehaviour {
 
     [SerializeField]
+	// constant time necessary to load a scene
     private float loadTime = 5f;
     private float timeElapsed;
 	
 	// Update is called once per frame
 	void Update () {
-
+	// ensure Unity has enough time to load
         timeElapsed += Time.deltaTime;
         if(timeElapsed > loadTime)
         {
