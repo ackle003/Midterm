fbf58f37c581dd021bcce7198708d79aaff0fcc2
ackle003 <50431311+ackle003@users.noreply.github.com>
2019-10-28 14:50:00 -0700

    Update DeathLoader.cs

--- GoToHellMidterm/Assets/Scripts/DeathLoader.cs
+++ GoToHellMidterm/Assets/Scripts/DeathLoader.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
+// Class which loads death screen upon failure
 public class DeathLoader : MonoBehaviour {
 
     private void OnTriggerEnter2D (Collider2D other)
