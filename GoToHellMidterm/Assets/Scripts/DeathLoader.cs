7c21b136918fea6dc55ff06524d0965c771c84b5
ackle003 <50431311+ackle003@users.noreply.github.com>
2019-10-28 15:59:53 -0700

    Update DeathLoader.cs

--- GoToHellMidterm/Assets/Scripts/DeathLoader.cs
+++ GoToHellMidterm/Assets/Scripts/DeathLoader.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
+//edit
 public class DeathLoader : MonoBehaviour {
 
     private void OnTriggerEnter2D (Collider2D other)
